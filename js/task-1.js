afcc58157d95ea3a5f0e60eb377519438a70c2ed
ouemdzi <sashka.hf@gmail.com>
2024-03-10 10:36:36 +0100

    task 1 done

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+// Задача 1. Генератор slug
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+
+function slugify(title) {
+  const replacedStr = title.replaceAll(' ', '-');
+  let slug = replacedStr.toLowerCase();
+  return slug;
+}
+
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
